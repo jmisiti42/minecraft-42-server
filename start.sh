993e192678a024720ad4f2c95cdebe15f7049126
Ownyd <33722819+Ownyd@users.noreply.github.com>
2019-11-06 14:39:48 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo '[Start] Server starting..';
-screen -S mcs java -Xms1G -Xmx3G -d64 -jar server.jar nogui
+screen -dm -S mcs java -Xms1G -Xmx3G -d64 -jar server.jar nogui
 echo '[Start] Server started..';
