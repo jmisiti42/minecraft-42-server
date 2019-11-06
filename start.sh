4951616af8bb7b3050fbfb28aeff170b4e3c1911
tlux2 <lux.thibault@hotmail.com>
2019-11-06 18:59:20 +0100

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-echo '[Start] Server starting..';
-screen -d -m -S mcs java -Xms1G -Xmx3G -d64 -jar server.jar nogui
-echo '[Start] Server launched successfully'
-
-echo '[Start] Sleeping - awaiting latest.log creation // TODO find a better way'
-sleep 5
-echo '[Start] Sleeping over'
-
-echo '[Start] Enabling logging'
-chmod 666 /dev/ttyS0
-screen -dm -S logs bash -c 'tail -f logs/latest.log > /dev/ttyS0'
-echo '[Start] Logger enabled'
-echo '[Start] Server started..';
+echo '[Start] Launching server with logger'
+screen -dm -S mcs bash -c 'java -Xms1G -Xmx3G -d64 -jar server.jar nogui > /dev/ttyS0'
+echo '[Start] Server and logger launched successfully'
