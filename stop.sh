010b329b5e86b30d054f23a87111331a124435d7
tlux2 <lux.thibault@hotmail.com>
2019-11-06 18:24:50 +0100

    update stop

--- stop.sh
+++ stop.sh
@@ -5,3 +5,9 @@ do
   kill $l
 done
 echo '[Shop] Java server(s) stopped..'
+echo '[Stop] Stopping every tail commands'
+for l in $(ps aux | grep "tail" | grep -v "grep" | tr -s ' ' '-' | cut -d'-' -f 2)
+do
+  kill $l
+done
+echo '[Shop] Tail commands stopped..'
